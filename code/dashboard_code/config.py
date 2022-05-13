8a1719b63d0f46c58cb9f833bc6471e2d434d562
Jed Dryer <dryer.jed@gmail.com>
2022-05-13 08:07:16 -0500

    reorganized and updated code

--- code/dashboard_code/config.py
+++
@@ -1,5 +0,0 @@
-database = "arctic_analysts_capstone"
-table = "table_name" # This will be variable
-user = "arctic_analysts"
-password  = "ThisPassw0rd!"
-server = "gen10-data-fundamentals-22-02-sql-server.database.windows.net"
